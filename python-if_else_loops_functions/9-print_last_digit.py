276a20841ca763ab73b02a811cf3b55302542fa4
mynameisnajaf <12692@holbertonstudents.com>
2025-11-28 17:59:22 +0000

    My Work

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    return abs(number)%10
+    if number >= 0:
+        last_digit = number % 10
+    elif number < 0:
+        last_digit = (number * -1) % 10
+
+    print(last_digit, end='')
+    return last_digit
