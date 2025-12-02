a0f646bfd56f4b62f619a94db4b4340c284e1a9b
mynameisnajaf <12692@holbertonstudents.com>
2025-12-02 13:16:25 +0000

    How to make a script dynamic!

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+if len(sys.argv) - 1 == 0:
+    print("{} arguments.".format(len(sys.argv) - 1))
+elif len(sys.argv) - 1 == 1:
+    print("{} argument:".format(len(sys.argv) - 1))
+else:
+    print("{} arguments:".format(len(sys.argv) - 1))
+for i, arg in enumerate(sys.argv[1:], start=1):
+    print("{}: {}".format(i, arg))
