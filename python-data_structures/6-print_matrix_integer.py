bc6ca2f601eb6a317f63bf3f079b8d85ae4e2b99
mynameisnajaf <12692@holbertonstudents.com>
2025-12-02 14:59:02 +0000

    Python - Data Structures: Lists, Tuples

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            print('{:d}'.format(matrix[i][j]), end=" ")
+    for row in matrix:
+        for j in range(len(row)):
+            if j != len(row) - 1:
+                print('{:d}'.format(row[j]), end=" ")
+            else:
+                print('{:d}'.format(row[j]), end="")
         print()
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
