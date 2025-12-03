2b180ea58834ca9bb4af507feb64243af866a162
mynameisnajaf <12692@holbertonstudents.com>
2025-12-03 09:53:27 +0000

    Python - More Data Structures: Set, Dictionary

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -4,12 +4,3 @@ def square_matrix_simple(matrix=[]):
     for i in matrix:
         new_matrix.append([x**2 for x in i])
     return new_matrix
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-new_matrix = square_matrix_simple(matrix)
-print(new_matrix)
-print(matrix)
