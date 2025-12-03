fe1d081c1476302365e164d1348682b7bd8244d0
mynameisnajaf <12692@holbertonstudents.com>
2025-12-03 13:08:31 +0000

    Python - More Data Structures: Set, Dictionary

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+       return None
+    max_score = 0
+    for key in a_dictionary:
+        if max_score < a_dictionary[key]:
+            max_score = a_dictionary[key]
+            max_key = key
+    return max_key
