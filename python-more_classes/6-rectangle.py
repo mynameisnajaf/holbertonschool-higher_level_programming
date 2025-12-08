c7c0f1a0823621377df1ce21bc3d1cbe390fead7
mynameisnajaf <12692@holbertonstudents.com>
2025-12-08 11:13:49 +0000

    ow many instances

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -11,7 +11,7 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-        self.number_of_instances += 1
+        type(self).number_of_instances += 1
 
     @property
     def width(self):
@@ -62,5 +62,5 @@ class Rectangle:
         return "Rectangle({}, {})".format(self.width, self.height)
 
     def __del__(self):
-        self.number_of_instances -= 1
+        type(self).number_of_instances -= 1
         print("{}".format("Bye rectangle..."))
