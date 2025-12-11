c62019aa87618029b01bafd969fd88624b3ebeff
mynameisnajaf <12692@holbertonstudents.com>
2025-12-11 10:27:24 +0000

    Integer validator

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Module that contains an error class"""
+
+
+class BaseGeometry:
+    """Contains a function to raise error"""
+    def area(self):
+        """Function that raises Exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """A function that validates value"""
+        if not isinstance(value, int):
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
