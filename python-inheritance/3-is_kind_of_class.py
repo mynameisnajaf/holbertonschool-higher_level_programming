843b22a7806c02e5a4947575fb0d2cc868035cca
mynameisnajaf <12692@holbertonstudents.com>
2025-12-11 10:05:03 +0000

    Same class or inherit from

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Module that contains a function to check isinstance"""
+
+
+def is_same_class(obj, a_class):
+    """Function to check obj is an instance of the specified class"""
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
