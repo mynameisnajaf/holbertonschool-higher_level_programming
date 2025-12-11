eb1725ec6766d849a10eea54e3c61caae4478870
mynameisnajaf <12692@holbertonstudents.com>
2025-12-11 11:04:18 +0000

    Square #1

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Defines a class Square that inherits from Rectangle."""
-Rectangle = __import__('9-rectangle.py').Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
