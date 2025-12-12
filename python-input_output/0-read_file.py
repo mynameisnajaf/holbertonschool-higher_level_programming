79cdc743e3131cfc5fa5ecd8c717d576ef3909b9
mynameisnajaf <12692@holbertonstudents.com>
2025-12-12 07:32:44 +0000

    Read file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Module that contains a function
+   to read file and print it"""
+
+
+def read_file(filename=""):
+    """Function to read and print file"""
+    with open("{}".format(filename), encoding="utf-8") as f:
+        f.read()
