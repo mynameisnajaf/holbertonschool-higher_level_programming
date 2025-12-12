d3b82c1b47c91f631b8875a83cd349e16a1201ef
mynameisnajaf <12692@holbertonstudents.com>
2025-12-12 07:40:35 +0000

    Write to a file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Module that contains a function
+   to write to file and return number of chars"""
+
+
+def write_file(filename="", text=""):
+    """Function to read and print file"""
+    with open("{}".format(filename), encoding="utf-8") as f:
+        return f.write("{}".format(text))
