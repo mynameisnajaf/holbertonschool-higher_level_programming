d586add3d827d0b9c61b6bdb49d7e0435ad89077
mynameisnajaf <12692@holbertonstudents.com>
2025-12-12 07:59:59 +0000

    Save to json file

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module that contains a function
+   to save to JSON file"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Function to save object to json file"""
+    with open("{}".format(filename), 'w') as json_file:
+        json.dump(my_obj, json_file, indent=4)
