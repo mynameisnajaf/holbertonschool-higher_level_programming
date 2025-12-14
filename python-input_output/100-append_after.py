5d09c837a0131691a6b608ad4f4d0b3703cde867
1244 <123>
2025-12-14 12:52:26 +0000

    My advanced work

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -4,7 +4,15 @@
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """A function that contains bla bla"""
-    with open("{}".format(filename), "a", encoding="utf-8") as f:
-        if search_string == new_string:
-            return f.write(search_string)
+    """A function that doesh smth"""
+    with open(filename, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+
+    new_lines = []
+    for line in lines:
+        new_lines.append(line)
+        if search_string in line:
+            new_lines.append(new_string)
+
+    with open(filename, "w", encoding="utf-8") as f:
+        f.writelines(new_lines)
