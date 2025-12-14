8e36ae6b4d2d65ac1d713f1ae40d52f056d3640b
1244 <123>
2025-12-14 12:42:32 +0000

    My advanced work

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -7,4 +7,4 @@ def append_after(filename="", search_string="", new_string=""):
     """A function that contains bla bla"""
     with open("{}".format(filename), "a") as f:
         if search_string == new_string:
-            f.write(new_string)
+            f.write(search_string)
