0403afc73bdd0bfcf62cda3c8f535a227e68dae5
abcdea <abcdea>
2025-12-18 07:10:06 +0000

    0. Basic Serialization

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -3,8 +3,11 @@ import pickle
 
 
 def serialize_and_save_to_file(data, filename):
-    with open(filename, "wb") as file:
-        pickle.dump(data, file)
+    try:
+        with open(filename, "wb") as file:
+            pickle.dump(data, file)
+    except (pickle.PicklingError, AttributeError) as e:
+        raise TypeError("Data is not serializable") from e
 
 def load_and_deserialize(filename):
     with open(filename, "rb") as file:
