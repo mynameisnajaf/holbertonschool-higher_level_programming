206a46408cb84dbbc3455e6b0f255630a587133b
aaa <aaa>
2025-12-18 11:12:17 +0000

    9.Full creation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,10 +1,12 @@
---creates a table second_table
-CREATE TABLE IF NOT EXISTS second_table(
-    id INT
-    name VARCHAR(256)
+-- Creates a table second_table
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(128),
     score INT
 );
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
+
+-- Insert the expected rows
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
