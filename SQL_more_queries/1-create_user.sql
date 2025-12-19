2b7b440e828ac61969f9a658c86e11c3dfdf1ed2
abcdea <abcdea>
2025-12-19 11:25:52 +0000

    <-_->

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -3,4 +3,4 @@ DROP USER IF EXISTS 'user_0d_1'@'localhost';
 
 CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
