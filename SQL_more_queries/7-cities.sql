e3110b0ad06561bb0b961136327f43f05b9cdfd8
abcdea <abcdea>
2025-12-19 12:24:40 +0000

    <-_->

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,7 @@
+-- creates the database hbtn_0d_usa and the table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+    id INT AUTO INCREMENT PRIMARY KEY,
+    FOREIGN KEY (state_id) REFERENCES states(id),
+    name VARCHAR(256) NOT NULL
+);
