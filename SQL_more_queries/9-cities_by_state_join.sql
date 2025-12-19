f7b4d427f41b320a762418b97fc633e9ff0a755f
abcdea <abcdea>
2025-12-19 12:45:35 +0000

    <-_->

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- Lists all cities contained in the database
 SELECT cities.id, cities.name, states.name
-FROM cities NATURAL JOIN states
-ORDER BY cities.id ASC; 
+FROM cities JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
