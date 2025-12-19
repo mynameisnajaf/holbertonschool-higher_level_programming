5a6079d5f96272d788e9b3949246bdc7e30089bb
abcdea <abcdea>
2025-12-19 13:24:33 +0000

    <-_->

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -2,6 +2,6 @@
 SELECT g.name AS name
 FROM tv_genres g
 JOIN tv_show_genres sg ON g.id = sg.genre_id
-JOIN tv_shows s ON s.id = sg.tv_show_id
+JOIN tv_shows s ON s.id = sg.show_id
 WHERE s.title = 'Dexter'
 ORDER BY name;
