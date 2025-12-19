838cba705ec1918c9b30bf13afd81740079d17ed
abcdea <abcdea>
2025-12-19 09:49:46 +0000

    14.Average

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,2 +1,2 @@
 -- Computes the score average of all records in the table
-INSERT INTO second_table (average) VALUES (SELECT AVG(score)) FROM second_table
+INSERT INTO second_table (average) SELECT AVG(score) FROM second_table;
